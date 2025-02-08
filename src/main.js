9a50dbce0a699e6a76f06badf09e3e2f008c565b
Ida <idazou85@gmail.com>
2025-02-09 06:11:32 +0800

    MeshStandardMaterial, DirectionalLight

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { BoxGeometry, Color, Mesh, MeshBasicMaterial, MeshStandardMaterial, PerspectiveCamera, Scene, WebGLRenderer } from "three"
+import { BoxGeometry, Color, DirectionalLight, DirectionalLightHelper, Mesh, MeshBasicMaterial, MeshStandardMaterial, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from "three"
 import { OrbitControls } from "three/examples/jsm/Addons.js"
-import { cameraViewMatrix, materialOpacity } from "three/tsl"
+import { cameraViewMatrix, color, materialOpacity } from "three/tsl"
 
 console.log("hello")
 
@@ -16,6 +16,8 @@ renderer.setViewport(0, 0, canvasElement.clientWidth, canvasElement.clientHeight
 //3D世界有場景和攝影機，要先畫攝影機才會有東西建置
 
 let camera = new PerspectiveCamera() //OrthographicCamera 
+camera.aspect = canvasElement.clientWidth / canvasElement.clientHeight
+camera.updateProjectionMatrix()
 // OrthographicCamera看不出距離感
 
 let scene = new Scene()
@@ -25,7 +27,7 @@ let mesh = new Mesh() //網格
 
 let geo = new BoxGeometry(1, 1, 1) //形狀
 
-let material = new MeshBasicMaterial() //材質，設定顏色
+let material = new MeshStandardMaterial() //材質，設定顏色 //MeshBasicMaterial不吃光線
 
 material.color = new Color(1, 0, 0)
 
@@ -34,6 +36,14 @@ mesh.geometry = geo
 mesh.material = material
 
 scene.add(mesh)
+let light = new DirectionalLight(new Color(1, 1, 1), 0.7) //直光
+// light.rotation.x  = light.rotation.x + 0.2
+// light.rotateX(90 * Math.PI / 180)
+light.position.set(5, 1, 2)
+light.lookAt(new Vector3(0,0,0))
+// scene.add(new DirectionalLightHelper(light))
+
+scene.add(light)
 
 camera.position.z = 5
 
@@ -42,13 +52,13 @@ let x = 0.01
 function loop() {
     renderer.clear()
     renderer.render(scene, camera)
-    if (material.color.r >0.7) {
+    if (material.color.r > 0.7) {
         x = -0.01
     }
     if (material.color.r < 0.1) {
         x = 0.01
     }
-    material.color.r = material.color.r + x
+    material.color.r += x
 }
 
 renderer.setAnimationLoop(loop)
